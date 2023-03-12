978726b604c7feeccba105bcbef2215a293a9cf3
sunxiangyu <andre.sun@liquiditytech.com>
2023-03-12 09:56:54 +0800

    数据库

--- src/main/java/com/example/springttest/Mapper/UserInfoMapper.java
+++ src/main/java/com/example/springttest/Mapper/UserInfoMapper.java
@@ -1,9 +1,10 @@
 package com.example.springttest.Mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.springttest.Entrity.UserInfoEntity;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-
-public class UserInfoMapper {
-    List<UserInfoEntity> userInfoEntities;
+@Mapper
+public interface UserInfoMapper extends BaseMapper<UserInfoEntity> {
 }
--- src/main/java/com/example/springttest/SpringtTestApplication.java
+++ src/main/java/com/example/springttest/SpringtTestApplication.java
@@ -1,9 +1,11 @@
 package com.example.springttest;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.springttest.Mapper")
 public class SpringtTestApplication {
 
     public static void main(String[] args) {
